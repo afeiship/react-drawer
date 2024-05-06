77abcbc4c673f29701a30d0bf2a7ae71fc3356ed
afeiship <1290657123@qq.com>
2024-05-06 20:52:31 +0800

    feat: init drawer impl

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -1,14 +1,33 @@
-import Button from '@jswork/react-drawer';
+import React from'react';
+import ReactDrawer from '@jswork/react-drawer/src';
 import './index.css';
-import '@jswork/react-drawer/dist/style.css';
+import '@jswork/react-drawer/src/style.scss';
 
 function App() {
+  const [visible, setVisible] = React.useState(false);
+
+  const handleOpen = () => {
+    setVisible(true);
+  };
+
+  const handleClose = () => {
+    setVisible(false);
+  };
+
   return (
-    <div className="m-10 p-4 shadow bg-gray-100 text-gray-800 hover:shadow-md transition-all">
-      <h1>react-drawer</h1>
-      <Button onClick={() => console.log('click me!')}>
-        Click me
-      </Button>
+    <div className="h-screen relative">
+      <button className="mt-5 bg-red-400 text-white p-2 w-1/4" onClick={handleOpen}>Open</button>
+      <ReactDrawer visible={visible} withBackdrop keepMounted closeOnBackdropClick onClose={handleClose} className="w-64 bg-gray-100 p-4">
+        <ul>
+          <li>道可道，非常道；名可名，非常名。</li>
+          <li>无名，天地之始，有名，万物之母。</li>
+          <li>故常无欲，以观其妙，常有欲，以观其徼。</li>
+          <li>此两者，同出而异名，同谓之玄，玄之又玄，众妙之门。</li>
+        </ul>
+        <nav>
+          <button className="bg-blue-400 text-white p-2 w-full" onClick={handleClose}>Close</button>
+        </nav>
+      </ReactDrawer>
     </div>
   );
 }
--- packages/lib/src/index.tsx
+++ packages/lib/src/index.tsx
@@ -1,15 +1,211 @@
-import React, { HTMLAttributes } from 'react';
+import noop from '@jswork/noop';
 import cx from 'classnames';
+import React, { Component, HTMLAttributes } from 'react';
+import VisibleElement, { VisibleState } from '@jswork/visible-element';
 
 const CLASS_NAME = 'react-drawer';
+const uuid = () => Math.random().toString(36).substring(2, 9);
 
-export type IProps = {
+export type ReactDialogProps = {
+  /**
+   * The extended className for component.
+   * @default ''
+   */
   className?: string;
-  children: React.ReactNode;
-  onClick?: () => void;
-} & HTMLAttributes<HTMLDivElement>;
+  /**
+   * The backdrop className.
+   * @default ''
+   */
+  backdropClassName?: string;
+  /**
+   * The dialog zIndex.
+   * @default 100
+   */
+  zIndex?: number;
+  /**
+   * The dialog unique name.
+   */
+  uuid?: string;
+  /**
+   * The dialog visible status.
+   */
+  visible: boolean;
+  /**
+   * The dialog close callback.
+   */
+  onClose: () => void;
+  /**
+   * Whether to show backdrop or not.
+   */
+  withBackdrop?: boolean;
+  /**
+   * Whether to fixed dialog or not.
+   */
+  fixed?: boolean;
+  /**
+   * Whether to keep dialog mounted or not.
+   */
+  keepMounted?: boolean;
+  /**
+   * Whether to close dialog on escape keydown.
+   */
+  closeOnEscape?: boolean;
+  /**
+   * Whether close dialog on click backdrop.
+   */
+  closeOnBackdropClick?: boolean;
+  /**
+   * The backdrop props.
+   */
+  backdropProps?: HTMLAttributes<HTMLDivElement>;
+} & HTMLAttributes<HTMLDialogElement> & React.RefAttributes<HTMLDialogElement>;
 
-export default function Button(props: IProps) {
-  const { className, children, ...rest } = props;
-  return <div data-component={CLASS_NAME} className={cx(CLASS_NAME, className)} {...rest} />;
+export default class ReactDrawer extends Component<ReactDialogProps> {
+  static displayName = CLASS_NAME;
+  static version = '__VERSION__';
+  static defaultProps = {
+    zIndex: 100,
+    visible: false,
+    fixed: false,
+    withBackdrop: false,
+    keepMounted: false,
+    closeOnEscape: false,
+    closeOnBackdropClick: false
+  };
+
+  private dialogRef = React.createRef<HTMLDialogElement>();
+  private backdropRef = React.createRef<HTMLDivElement>();
+  private uuid = this.props.uuid || `${CLASS_NAME}-${uuid()}`;
+  private veDialog: VisibleElement;
+  private veBackdrop: VisibleElement;
+
+  // ---- dom elements ----
+  get dialog() {
+    return this.dialogRef.current as HTMLDialogElement;
+  }
+
+  get dialogStyle() {
+    const { zIndex, style } = this.props;
+    return { zIndex, ...style } as React.StyleHTMLAttributes<HTMLDialogElement>;
+  }
+
+  get backdrop() {
+    return this.backdropRef.current as HTMLDivElement;
+  }
+
+  get backdropStyle() {
+    const { zIndex, style } = this.props;
+    return { zIndex: zIndex! - 1, ...style } as React.StyleHTMLAttributes<HTMLDivElement>;
+  }
+
+  // ---- state react ----
+  state = {
+    animateVisible: this.props.visible
+  };
+
+  // ---- life cycle start ----
+  constructor(props: ReactDialogProps) {
+    super(props);
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentDidMount() {
+    const { visible } = this.props;
+    if (visible) this.present();
+    this.veDialog = new VisibleElement(this.dialog, { onChange: this.handleVeChange });
+    this.veBackdrop = new VisibleElement(this.backdrop);
+  }
+
+  shouldComponentUpdate(nextProps: ReactDialogProps): boolean {
+    const { visible } = nextProps;
+    if (visible) this.present();
+    if (!visible) this.dismiss();
+    return true;
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  // ---- life cycle end ----
+
+  // ---- public methods ----
+  present = () => {
+    this.veDialog.show();
+    this.veBackdrop.show();
+  };
+
+  dismiss = () => {
+    const { onClose } = this.props;
+    this.veDialog.close();
+    this.veBackdrop.close();
+    onClose?.();
+  };
+
+  handleVeChange = (state: VisibleState) => {
+    if (state === 'show') this.setState({ animateVisible: true });
+    if (state === 'closed') this.setState({ animateVisible: false });
+  };
+
+  handleKeyDown = (event: KeyboardEvent) => {
+    const { closeOnEscape } = this.props;
+    const { key } = event;
+    if (!closeOnEscape) return;
+    if ('Escape' === key) this.dismiss();
+  };
+
+  render() {
+    const {
+      className,
+      backdropClassName,
+      visible,
+      withBackdrop,
+      zIndex,
+      fixed,
+      children,
+      keepMounted,
+      closeOnEscape,
+      closeOnBackdropClick,
+      backdropProps,
+      onClose,
+      ...dialogProps
+    } = this.props;
+
+    const { animateVisible } = this.state;
+    const keepChildren = keepMounted || animateVisible;
+
+    return (
+      <>
+        <dialog
+          id={this.uuid}
+          role="drawer"
+          aria-modal="true"
+          data-component={CLASS_NAME}
+          data-fixed={fixed}
+          className={cx(CLASS_NAME, className)}
+          ref={this.dialogRef}
+          style={this.dialogStyle}
+          {...dialogProps}
+        >
+          {keepChildren ? children : null}
+        </dialog>
+
+        {
+          withBackdrop && (
+            <div
+              id={`${this.uuid}-backdrop`}
+              role="backdrop"
+              aria-hidden="true"
+              hidden
+              className={cx(`${CLASS_NAME}__backdrop`, backdropClassName)}
+              ref={this.backdropRef}
+              onClick={closeOnBackdropClick ? this.dismiss : noop}
+              style={this.backdropStyle}
+              {...backdropProps}
+            />
+          )
+        }
+      </>
+    );
+  }
 }
